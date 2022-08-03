4552787a859ebdacb43a688f94ae10f66fbb8e46
jude-m <jude.mahipalamudali@outlook.com>
2022-08-03 09:51:22 +0530

    added more test coverage

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,8 +1,11 @@
+"use strict";
 import request from 'supertest';
-const baseURL = "localhost:3000/api";
+const baseURL = "http://ec2-13-235-76-19.ap-south-1.compute.amazonaws.com:3000/api";
+
+//I havent mocked the API calls, directly using the API. will do as the next step.
 
 describe("GET/ledger - Basic suite", () => {
-    it("should return 200 and return data", async () => {
+    it("Normal request", async () => {
         const response = await request(baseURL)
             .get("/ledger")
             .query({
@@ -13,17 +16,79 @@ describe("GET/ledger - Basic suite", () => {
                 "timezone": "America/Los_Angeles"
             });
         expect(response.status).toBe(200);
-        expect(response.body.length>=0).toBe(true);
+        expect(response.body.length >= 0).toBe(true);
     });
-    it("should return 400", async () => {
+    it("Empty string for start_date - ", async () => {
         const response = await request(baseURL).get("/ledger")
-        .query({
-            "start_date": "",
-            "end_date": "2023-05-27",
-            "weekly_rent": "555",
-            "frequency": "FORTNIGHTLY",
-            "timezone": "America/Los_Angeles"
-        }); 
+            .query({
+                "start_date": "",
+                "end_date": "2023-05-27",
+                "weekly_rent": "555",
+                "frequency": "FORTNIGHTLY",
+                "timezone": "America/Los_Angeles"
+            });
         expect(response.status).toBe(400);
+        console.log("Empty string for start_date - " + response.text);
+    });
+    it("end_date not in UTC format", async () => {
+        const response = await request(baseURL).get("/ledger")
+            .query({
+                "start_date": "2023-03-28",
+                "end_date": "18-05-2023",
+                "weekly_rent": "555",
+                "frequency": "FORTNIGHTLY",
+                "timezone": "America/Los_Angeles"
+            });
+        expect(response.status).toBe(400);
+        console.log("end_date not in UTC format - " + response.text);
+    });
+    it("start_date cannot be greater than end_date", async () => {
+        const response = await request(baseURL)
+            .get("/ledger")
+            .query({
+                "start_date": "2024-03-28",
+                "end_date": "2023-05-27",
+                "weekly_rent": "555",
+                "frequency": "FORTNIGHTLY",
+                "timezone": "America/Los_Angeles"
+            });
+        expect(response.status).toBe(400);
+        console.log("start_date cannot be greater than end_date - " + response.text);
+    });
+    it("Negative week_rent", async () => {
+        const response = await request(baseURL).get("/ledger")
+            .query({
+                "start_date": "2023-03-28",
+                "end_date": "2023-05-27",
+                "weekly_rent": "-50",
+                "frequency": "FORTNIGHTLY",
+                "timezone": "America/Los_Angeles"
+            });
+        expect(response.status).toBe(400);
+        console.log("Negative week_rent - " + response.text);
+    });
+    it("Invalid frequency", async () => {
+        const response = await request(baseURL).get("/ledger")
+            .query({
+                "start_date": "2023-03-28",
+                "end_date": "2023-05-27",
+                "weekly_rent": "555",
+                "frequency": "YEARLY",
+                "timezone": "America/Los_Angeles"
+            });
+        expect(response.status).toBe(400);
+        console.log("Invalid frequency - " + response.text);
+    });
+    it("Invalid timezone", async () => {
+        const response = await request(baseURL).get("/ledger")
+            .query({
+                "start_date": "2023-03-28",
+                "end_date": "2023-05-27",
+                "weekly_rent": "555",
+                "frequency": "MONTHLY",
+                "timezone": "America/LosAngeles"
+            });
+        expect(response.status).toBe(400);
+        console.log("Invalid timezone - " + response.text);
     });
 });
--- tests/date-utils.test.js
+++ tests/date-utils.test.js
@@ -1,21 +1,85 @@
+"use strict";
 import * as dateUtils from '../src/utils/date-utils.js';
 
 describe("Testing addMonth()", () => {
-    it("Testing generic test case", () => {
-        const input = new Date('2023-01-28');
-        const output = new Date ('2023-02-28');
-        expect(dateUtils.addMonth(input)).toEqual(output);
+    it("Addng a month", () => {
+        const inputDate = new Date('2023-01-28');
+        const outputDate = new Date('2023-02-28');
+        expect(dateUtils.addMonth(inputDate)).toEqual(outputDate);
     });
 
-    it("Testing adding a month for 31st", () => {
-        const input = new Date('2023-08-31');
-        const output = new Date ('2023-10-01') ;
-        expect(dateUtils.addMonth(input)).toEqual(output);
+    it("Adding a month on the last day - 31st", () => {
+        const inputDate = new Date('2023-08-31');
+        const outputDate = new Date('2023-10-01');
+        expect(dateUtils.addMonth(inputDate)).toEqual(outputDate);
     });
 
-    it("Testing an incorrect date input", () => {
-        const input = new Date('2023-01-28ss');
-        const output = new Date (NaN);
-        expect(JSON.stringify(input)).toEqual(JSON.stringify(output));
+    it("Invalid date input", () => {
+        const inputDate = new Date('2023-01-28ss');
+        expect(dateUtils.addMonth(inputDate)).toBeUndefined();
+    });
+})
+
+describe("Testing addDays()", () => {
+    it("Addng days", () => {
+        const inputDate = new Date('2023-01-28');
+        const inputDays = 6;
+        const outputDate = new Date('2023-02-03');
+        expect(dateUtils.addDays(inputDate, inputDays)).toEqual(outputDate);
+    });
+    it("Addng days in a leap year on the last day of Feb", () => {
+        const inputDate = new Date('2024-02-28');
+        const inputDays = 2;
+        const outputDate = new Date('2024-03-01');
+        expect(dateUtils.addDays(inputDate, inputDays)).toEqual(outputDate);
+    });
+    it("Addng zero days", () => {
+        const inputDate = new Date('2025-02-28');
+        const inputDays = 0;
+        const outputDate = new Date('2025-02-28');
+        expect(dateUtils.addDays(inputDate, inputDays)).toEqual(outputDate);
+    });
+    it("Adding negative days", () => {
+        const inputDate = new Date('2025-02-28');
+        const inputDays = -5;
+        const outputDate = new Date('2025-02-23');
+        expect(dateUtils.addDays(inputDate, inputDays)).toEqual(outputDate);
+    });
+    it("Skipping inputDays", () => {
+        const inputDate = new Date('2025-02-28');
+        const inputDays = -5;
+        const outputDate = new Date('2025-02-28');
+        expect(dateUtils.addDays(inputDate)).toEqual(outputDate);
+    });
+    it("Invalid date input", () => {
+        const inputDate = new Date('2025-02-28xxx');
+        const inputDays = 4;
+        expect(dateUtils.addDays(inputDate, inputDays)).toBeUndefined();
+    });
+})
+
+describe("Testing getDateDiffInDays()", () => {
+    it("Postive difference", () => {
+        const date1 = new Date('2023-03-01').getTime();
+        const date2 = new Date('2023-02-24').getTime();
+        expect(dateUtils.getDateDiffInDays(date1, date2)).toBe(5);
+    });
+
+    it("Negative difference", () => {
+        const date1 = new Date('2023-02-01').getTime();
+        const date2 = new Date('2023-02-10').getTime();
+        expect(dateUtils.getDateDiffInDays(date1, date2)).toBe(-9);
+    });
+
+    it("Difference between the same day", () => {
+        const date1 = new Date().getTime();
+        const date2 = new Date().getTime();
+        expect(dateUtils.getDateDiffInDays(date1, date2)).toBe(0);
+    });
+
+    it("Invalid date input", () => {
+        const date1 = new Date('2023-01-28sss').getTime();
+        const date2 = new Date('2023-02-10').getTime();
+        expect(dateUtils.getDateDiffInDays(date1, date2)).toBeUndefined();
     });
 })
